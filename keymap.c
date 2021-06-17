f780c76d18d1ad874a820ef5052012f703633da0
hmngwy <1647347+hmngwy@users.noreply.github.com>
2021-06-17 23:20:38 +0800

    opt

--- keymap.c
+++ keymap.c
@@ -52,23 +52,23 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   // LT_HOM
   [_SYM] = LAYOUT_3x5(
-    KC_CIRC, KC_GRV,  KC_TILD, KC_DLR,  KC_PERC,    KC_LPRN,  KC_RPRN,  KC_EQL,  KC_LT,   KC_GT,
-    KC_AT,   KC_HASH, KC_EXLM, KC_QUES, KC_AMPR,    KC_LCBR,  KC_RCBR,  KC_COLN, KC_LBRC, KC_RBRC,
-    ___,     ___,     ___,     KC_SCLN, KC_PIPE,    KC_UNDS,  KC_PAST,  KC_PMNS, KC_PPLS, ___,
+    KC_CIRC, KC_GRV,  KC_TILD, KC_DLR,  ___,        ___,     KC_LT,    KC_GT,   KC_LBRC, KC_RBRC,
+    KC_AT,   KC_HASH, KC_EXLM, KC_QUES, KC_AMPR,    KC_EQL,  KC_LPRN,  KC_RPRN, KC_LCBR, KC_RCBR,
+    ___,     KC_PAST, KC_PERC, KC_SCLN, KC_PIPE,    KC_UNDS, KC_COLN,  KC_PMNS, KC_PPLS, ___,
                       _v_,     _v_,     _v_,        _v_,      _v_,      _v_
   ),
 
   // RT_HOM
   [_NAV] = LAYOUT_3x5(
-    KC_PPLS, KC_7,    KC_8,     KC_9,     KC_PERC,       KC_PGUP,  KC_HOME,  KC_UP,    KC_END,  KC_PSCR,
-    KC_PMNS, KC_4,    KC_5,     KC_6,     KC_DOT,        KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_INS,
-    KC_PAST, KC_1,    KC_2,     KC_3,     KC_COMM,       ___,      ___,      ___,      ___,     ___,
+    KC_PPLS, KC_7,    KC_8,     KC_9,     KC_PERC,      KC_PGUP,  KC_HOME,  KC_UP,    KC_END,  KC_PSCR,
+    KC_PMNS, KC_4,    KC_5,     KC_6,     KC_DOT,       KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT, KC_INS,
+    KC_PAST, KC_1,    KC_2,     KC_3,     KC_COMM,      ___,      ___,      ___,      ___,     ___,
                       DF(_NAV), KC_0,     _v_,          _v_,      _v_,      DF(0)
   ),
 
   // RT_INN
   [_FUN] = LAYOUT_3x5(
-    KC_BRIU, KC_BRID, ___,      KC_F11,     KC_F12,        KC_WH_U,  KC_MS_BTN1, KC_MS_U, KC_MS_BTN2, RESET,
+    KC_BRIU, KC_BRID, KC_F11,   KC_F12,     ___,           KC_WH_U,  KC_MS_BTN1, KC_MS_U, KC_MS_BTN2, RESET,
     KC_F1,   KC_F2,   KC_F3,    KC_F4,      KC_F5,         KC_WH_D,  KC_MS_L,    KC_MS_D, KC_MS_R,    ___,
     KC_F6,   KC_F7,   KC_F8,    KC_F9,      KC_F10,        KC_VOLD,  KC_VOLU,    KC_MPLY, KC_MPRV,    KC_MNXT,
                       DF(_FUN), KC_MS_BTN1, KC_MS_BTN2,    _v_,      _v_,        DF(0)
@@ -121,6 +121,15 @@ bool get_tapping_force_hold(uint16_t keycode, keyrecord_t *record) {
 
 
 #ifdef OLED_DRIVER_ENABLE
+
+#define L_BASE 0
+#define L_SYM 1
+#define L_NAV 2
+#define L_FUN 3
+#define L_PAD 4
+#define L_VAL 5
+#define L_LOC 6
+
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   if (!is_keyboard_master()) {
     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
@@ -128,187 +137,103 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
   return rotation;
 }
 
-#define L_BASE 0
-#define L_LOWER 2
-#define L_RAISE 4
-#define L_ADJUST 8
-
 void oled_render_layer_state(void) {
-
     oled_write_P(PSTR("Layer: "), false);
-
     switch (layer_state) {
-
         case L_BASE:
-
             oled_write_ln_P(PSTR("Default"), false);
-
             break;
-
-        case L_LOWER:
-
-            oled_write_ln_P(PSTR("Lower"), false);
-
+        case L_SYM:
+            oled_write_ln_P(PSTR("Symbol"), false);
             break;
-
-        case L_RAISE:
-
-            oled_write_ln_P(PSTR("Raise"), false);
-
+        case L_NAV:
+            oled_write_ln_P(PSTR("Number/Navigate"), false);
             break;
-
-        case L_ADJUST:
-
-        case L_ADJUST|L_LOWER:
-
-        case L_ADJUST|L_RAISE:
-
-        case L_ADJUST|L_LOWER|L_RAISE:
-
-            oled_write_ln_P(PSTR("Adjust"), false);
-
+        case L_FUN:
+            oled_write_ln_P(PSTR("Function/Mouse"), false);
             break;
-
+        case L_PAD:
+            oled_write_ln_P(PSTR("Configure"), false);
+            break;
+        case L_VAL:
+            oled_write_ln_P(PSTR("Valorant"), false);
+            break;
+        case L_LOC:
+            oled_write_ln_P(PSTR("Locked"), false);
+            break;
+	default:
+            oled_write_ln_P(PSTR(layer_state), false);
+            break;
     }
-
 }
-
-
-
-
 
 char keylog_str[24] = {};
 
-
-
 const char code_to_name[60] = {
-
     ' ', ' ', ' ', ' ', 'a', 'b', 'c', 'd', 'e', 'f',
-
     'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-
     'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-
     '1', '2', '3', '4', '5', '6', '7', '8', '9', '0',
-
     'R', 'E', 'B', 'T', '_', '-', '=', '[', ']', '\\',
-
     '#', ';', '\'', '`', ',', '.', '/', ' ', ' ', ' '};
 
-
-
 void set_keylog(uint16_t keycode, keyrecord_t *record) {
-
   char name = ' ';
-
     if ((keycode >= QK_MOD_TAP && keycode <= QK_MOD_TAP_MAX) ||
-
         (keycode >= QK_LAYER_TAP && keycode <= QK_LAYER_TAP_MAX)) { keycode = keycode & 0xFF; }
-
   if (keycode < 60) {
-
     name = code_to_name[keycode];
-
   }
 
-
-
   // update keylog
-
   snprintf(keylog_str, sizeof(keylog_str), "%dx%d, k%2d : %c",
-
            record->event.key.row, record->event.key.col,
-
            keycode, name);
-
 }
-
-
 
 void oled_render_keylog(void) {
-
     oled_write(keylog_str, false);
-
 }
 
-
-
 void render_bootmagic_status(bool status) {
-
     /* Show Ctrl-Gui Swap options */
-
     static const char PROGMEM logo[][2][3] = {
-
         {{0x97, 0x98, 0}, {0xb7, 0xb8, 0}},
-
         {{0x95, 0x96, 0}, {0xb5, 0xb6, 0}},
-
     };
 
     if (status) {
-
         oled_write_ln_P(logo[0][0], false);
-
         oled_write_ln_P(logo[0][1], false);
-
     } else {
-
         oled_write_ln_P(logo[1][0], false);
-
         oled_write_ln_P(logo[1][1], false);
-
     }
-
 }
-
-
 
 void oled_render_logo(void) {
-
     static const char PROGMEM crkbd_logo[] = {
-
         0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94,
-
         0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4,
-
         0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4,
-
         0};
-
     oled_write_P(crkbd_logo, false);
-
 }
-
-
 
 void oled_task_user(void) {
-
     if (is_keyboard_master()) {
-
         oled_render_layer_state();
-
         oled_render_keylog();
-
     } else {
-
         oled_render_logo();
-
     }
-
 }
 
-
-
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-
   if (record->event.pressed) {
-
     set_keylog(keycode, record);
-
   }
-
   return true;
-
 }
 
 #endif // OLED_DRIVER_ENABLE
